68f89fd5c3a5f0b68aee6541322fbebfb678b0d9
Kewyn Ferreira <47131740+kewynf@users.noreply.github.com>
2022-03-02 01:03:33 -0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 export const Env = {
-  API_HOST: process.env.REACT_APP_API_HOST || 'https://booking.br.ivao.aero/api',
-  LOGO_URL: process.env.REACT_APP_LOGO_URL,
+  API_HOST: process.env.REACT_APP_API_HOST || 'https://api.kronos.staging.br.ivao.aero',
+  LOGO_URL: process.env.REACT_APP_LOGO_URL || 'https://assets.br.ivao.aero/logos/tag_white.png',
   DEV: Boolean(process.env.REACT_APP_DEV) || false,
 };
