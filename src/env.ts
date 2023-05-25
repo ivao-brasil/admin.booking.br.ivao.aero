7abba6b1103b35ab739d41b4e3d836aaf7456e15
Bruno Ricci <brunoriccidossantos@hotmail.com>
2023-05-24 22:06:04 -0300

    refactor(env): refact env to allow a convivence between models
    recreate environment variable LOGO_URL in order to allow a convivence between the new model of env and the old one.

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,10 @@
+const assetsHost = process.env.REACT_APP_ASSETS_HOST || 'https://assets.br.ivao.aero';
+
 export const Env = {
   LOGIN_HOST: process.env.REACT_APP_LOGIN_HOST || 'https://login.ivao.aero',
   API_HOST: process.env.REACT_APP_API_HOST || 'https://api.kronos.br.ivao.aero',
-  ASSETS_HOST: process.env.REACT_APP_ASSETS_HOST || 'https://assets.br.ivao.aero',
+  ASSETS_HOST: assetsHost,
+  LOGO_URL: process.env.REACT_APP_LOGO_URL || `${assetsHost}/logos/tag_white.png`,
   MAIN_SYSTEM_URL: process.env.REACT_APP_MAIN_SYSTEM_URL || 'https://kronos.br.ivao.aero',
   DEV: Boolean(process.env.REACT_APP_DEV) || false,
 };
